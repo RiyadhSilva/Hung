cfb1c687f55cf465b9985d34215b07a30b3e5d4d
RiyadhSilva <riyadhlevi@gmail.com>
2016-10-03 13:45:50 -0300

    Implementação - ajustes IniciaIntentService
    Correção de erro em relação ao cronômetro, agora ele está contando o
    tempo de forma correta.

--- app/src/main/java/com/example/riyad/hung/IniciaIntentService.java
+++ app/src/main/java/com/example/riyad/hung/IniciaIntentService.java
@@ -4,6 +4,8 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -21,31 +23,61 @@ public class IniciaIntentService extends IntentService{
     private static final String TAG = "livro";
     private boolean running;
     private String nome;
+    private DateFormat df;
+    private Date date;
 
     @Override
     protected void onHandleIntent(Intent intent){
+        df = new SimpleDateFormat("HH");
+        date = new Date();
+        String sHoraAgora = df.format(date);
+        int hora_agora = Integer.parseInt(sHoraAgora);
+        df = new SimpleDateFormat("mm");
+        String sMinutoAgora = df.format(date);
+        int minuto_agora = Integer.parseInt(sMinutoAgora);
+        System.out.println(hora_agora);
         nome = intent.getStringExtra("nome");
-        String hora = intent.getStringExtra("hora");
-        Long lHora = Long.parseLong(hora);
-        String minutos = intent.getStringExtra("minutos");
-        Long lMinutos = Long.parseLong(minutos);
+        String sHoraProgramada = intent.getStringExtra("hora");
+        int hora_programada = Integer.parseInt(sHoraProgramada);
+        String sMinutosProgramados = intent.getStringExtra("minutos");
+        int minutos_programados = Integer.parseInt(sMinutosProgramados);
+        Long timer;
+        Long programado;
 
-
-        Long duracao = ((lMinutos*60)) * 1000;
-        Long agora = (System.currentTimeMillis());
-        Long futuro = (agora + duracao)/1000;
-
-        running = true;
-        //Este metodo executa em uma thread
-        //Quando ele terminar, o metodo stopSelf() sera chamado automaticamente
-        Long count = agora/1000;
-        int time = 0;
-        while (running && time < 60){
-            fazAlgumaCoisa();
-            Log.d(TAG, "IniciaIntentService executando... " + time);
-            count++;
-            time++;
+        //Este metodo executa em uma thread depedendo do caso
+        if((hora_programada - hora_agora) == 0){
+            System.out.println("Caso 1 = 0");
+            running = true;
+            timer = System.currentTimeMillis() / 1000;
+            programado = timer + ((minutos_programados - minuto_agora) * 60);
+            Long sub = programado - timer;
+            System.out.println("Subtracao = " + sub);
+            while(timer < programado){
+                fazAlgumaCoisa();
+                Log.d(TAG, "IniciaIntentService executando... " + sub);
+                sub--;
+                timer++;
+            }
+        } else if (hora_programada - hora_agora > 0){
+            System.out.println("Caso 2 > 0");
+            running = true;
+            timer = System.currentTimeMillis() / 1000;
+            programado = timer + ((minutos_programados - minuto_agora) * 60) + ((hora_programada-hora_agora) * 60 * 60);
+            Long sub = programado - timer;
+            while (timer < programado){
+                fazAlgumaCoisa();
+                Log.d(TAG, "IniciaIntentService executando... " + sub);
+                sub--;
+                timer++;
+            }
+        } else if (hora_programada - hora_agora < 0){
+            System.out.println("Caso 3 < 0");
+            running = true;
+            System.out.print("Horário errado!");
+            Log.d(TAG, "IniciaIntentService erro = Horario invalido!... ");
         }
+
+        //Quando ele terminar, o metodo stopSelf() sera chamado automaticamente
         Log.d(TAG, "IniciaIntentService fim.");
     }
 
